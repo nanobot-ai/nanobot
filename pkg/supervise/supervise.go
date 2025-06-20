0aca579e6fd9d70a134a4b17e7d148dde5ae456f
Donnie Adams <donnie@acorn.io>
2025-06-19 22:20:01 -0400

    fix: ensure stdio server is killed when context is canceled
    Signed-off-by: Donnie Adams <donnie@acorn.io>

--- pkg/supervise/supervise.go
+++ pkg/supervise/supervise.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"os/exec"
 	"runtime"
+	"syscall"
 	"time"
 
 	"github.com/nanobot-ai/nanobot/pkg/system"
@@ -12,7 +13,30 @@ import (
 
 func Cmd(ctx context.Context, command string, args ...string) *exec.Cmd {
 	args = append([]string{"_exec", command}, args...)
-	return exec.CommandContext(ctx, system.Bin(), args...)
+	cmd := exec.CommandContext(ctx, system.Bin(), args...)
+
+	// Set up process group for proper cleanup
+	if runtime.GOOS != "windows" {
+		cmd.SysProcAttr = &syscall.SysProcAttr{
+			Setpgid: true,
+		}
+		cmd.Cancel = func() error {
+			if cmd.Process != nil {
+				// Kill the entire process group
+				return syscall.Kill(-cmd.Process.Pid, syscall.SIGKILL)
+			}
+			return nil
+		}
+	} else {
+		cmd.Cancel = func() error {
+			if cmd.Process != nil {
+				return cmd.Process.Kill()
+			}
+			return nil
+		}
+	}
+
+	return cmd
 }
 
 func Daemon() error {
@@ -22,11 +46,14 @@ func Daemon() error {
 	cmd := exec.CommandContext(ctx, os.Args[2], os.Args[3:]...)
 	cmd.Stderr = os.Stderr
 	cmd.Stdout = os.Stdout
+
+	// Inherit the process group from parent (don't create new one)
+	// The parent already set up the process group in Cmd()
 	cmd.Cancel = func() error {
-		if runtime.GOOS == "windows" {
+		if cmd.Process != nil {
 			return cmd.Process.Kill()
 		}
-		return cmd.Process.Signal(os.Interrupt)
+		return nil
 	}
 
 	processIn, err := cmd.StdinPipe()
