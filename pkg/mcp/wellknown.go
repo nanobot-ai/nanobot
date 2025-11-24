1d1a104e0d44ff34c3d8ba4502fd05db0b3712be
Donnie Adams <donnie@obot.ai>
2025-11-24 14:34:53 -0500

    fix: properly handle oauth protected resource

--- pkg/mcp/wellknown.go
+++ pkg/mcp/wellknown.go
@@ -3,7 +3,6 @@ package mcp
 import (
 	"encoding/json"
 	"net/http"
-	"strings"
 )
 
 func (h *HTTPServer) protectedMetadata(w http.ResponseWriter, r *http.Request) {
@@ -14,7 +13,6 @@ func (h *HTTPServer) protectedMetadata(w http.ResponseWriter, r *http.Request) {
 	}
 
 	protectedResourceMetadata := *h.protectedResourceMetadata
-	protectedResourceMetadata.Resource = strings.Replace(r.URL.String(), "/.well-known/oauth-protected-resource", "", 1)
 
 	w.Header().Set("Content-Type", "application/json")
 	if err := json.NewEncoder(w).Encode(protectedResourceMetadata); err != nil {
