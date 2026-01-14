50570c3243362c843beffff81c7ad8f43cbab00d
Donnie Adams <donnie@obot.ai>
2026-01-14 08:52:33 -0500

    Fix lint
    Signed-off-by: Donnie Adams <donnie@obot.ai>

--- packages/tasks/server.ts
+++ packages/tasks/server.ts
@@ -3,20 +3,20 @@ import ExecuteTaskStep from "./src/tools/execute-task-step.ts";
 import TaskStepStatus from "./src/tools/task-step-status.ts";
 
 const server = new Server(
-  {
-    name: "Nanobot Tasks",
-    version: "0.0.0",
-  },
-  {
-    tools: {
-      ExecuteTaskStep,
-      TaskStepStatus,
-    },
-  },
+	{
+		name: "Nanobot Tasks",
+		version: "0.0.0",
+	},
+	{
+		tools: {
+			ExecuteTaskStep,
+			TaskStepStatus,
+		},
+	},
 );
 
 export default server;
 
 if (import.meta.main) {
-  await server.serve(9014);
+	await server.serve(9014);
 }
