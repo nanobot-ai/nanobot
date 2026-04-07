aee1a5ea875c7a33079518772093286d268ab060
Calvin McLean <calvin@obot.ai>
2026-04-07 16:24:01 -0700

    fmt

--- pkg/types/dialect.go
+++ pkg/types/dialect.go
@@ -3,9 +3,9 @@ package types
 type Dialect string
 
 const (
-	DialectAnthropicMessages      Dialect = "AnthropicMessages"
-	DialectOpenAIResponses        Dialect = "OpenAIResponses"
-	DialectOpenResponses          Dialect = "OpenResponses"
-	DialectOpenAIChatCompletions  Dialect = "OpenAIChatCompletions"
-	DialectDefault                        = DialectOpenAIResponses
+	DialectAnthropicMessages     Dialect = "AnthropicMessages"
+	DialectOpenAIResponses       Dialect = "OpenAIResponses"
+	DialectOpenResponses         Dialect = "OpenResponses"
+	DialectOpenAIChatCompletions Dialect = "OpenAIChatCompletions"
+	DialectDefault                       = DialectOpenAIResponses
 )
